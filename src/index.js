5b212f7f004f1aa098309b8414d5aea77597f61c
RAZIA389 <106535085+RAZIA389@users.noreply.github.com>
2022-06-05 16:39:10 +0530

    mongoose-schema

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
+mongoose.connect( "mongodb+srv://raziaperwez38:48Vny2gtwyeSFdMo@functionup1.txsq8.mongodb.net/bookschema12",{useNewUrlParser: true})
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
---
+++ src/models/bookModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema( {
+    bookName: String,
+    authorName: String,
+    category: String,    
+    publishYear: Number
+}, { timestamps: true });
+
+module.exports = mongoose.model('Book', bookSchema) //books
+
+
+

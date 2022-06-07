9034004202615cd85d9965b350f6db70bd2a9f07
RAZIA389 <106535085+RAZIA389@users.noreply.github.com>
2022-06-07 09:29:43 +0530

    mongodb-assignment-2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect( "mongodb+srv://raziaperwez38:48Vny2gtwyeSFdMo@functionup1.txsq8.mongodb.net/mongobdassignment", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -17,6 +17,6 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3050, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3050))
 });
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -3,29 +3,15 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema( {
     bookName: String, 
     authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
+    tags: [ String ],
+     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    publishedYear: {type: Number, default: 2021},
+   totalPages:  Number ,
+   stock: Boolean
 }, { timestamps: true });
 
 
 module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
